7f23eed1be8344259be92f4f27b182bc6e903b0f
Avinash Kumar GolU <56521625+avinashssm@users.noreply.github.com>
2020-06-27 21:28:06 +0530

    Task_Complete

--- Hour_9_Scripting_Part_2/Assets/Scripts/CubeControlScript.cs
+++ Hour_9_Scripting_Part_2/Assets/Scripts/CubeControlScript.cs
@@ -2,15 +2,19 @@
 
 public class CubeControlScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    float itemscale = 1;
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(Input.GetAxis("Horizontal") * 5f, 0f, Input.GetAxis("Vertical") * 5f, 0f);
 
+        transform.Rotate(Input.GetAxis("Mouse Y") * 3f, -Input.GetAxis("Mouse X") * 3f, 0f);
+
+        if (Input.GetKey(KeyCode.N))
+            itemscale = .1f;
+        else if (Input.GetKey(KeyCode.M))
+            itemscale += .1f;
+        transform.localScale = new Vector3(itemscale, itemscale, itemscale);
     }
 }
